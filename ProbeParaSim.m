52eef1956f5390fb5b5a10db719a3666f5cbf953
rdeshmu1 <56365305+rdeshmu1@users.noreply.github.com>
2019-11-05 21:09:35 -0500

    Update ProbeParaSim.m

--- ProbeParaSim.m
+++ ProbeParaSim.m
@@ -4,7 +4,7 @@ clear; clc; close all;
 %%
 %use linear density model
 
-g= 9.81; %10.44; % gravity on Saturn (kg/m2)
+g= 10.44; %10.44; % gravity on Saturn (kg/m2)
 z=72000000; %iterations
 deltat=.0001; %time step (s)
 time_seconds=deltat*z; %time in seconds
@@ -13,7 +13,7 @@ time_hours = time_seconds/60/60; %time in hours
 %%
 %pre/post parachute deployment
 
-ripcord=10; %ripcord length (m)
+ripcord=500; %ripcord length (m)
 Torque = 300; %torque of spool (Nm)
 R_spool = .3; %radius of spool (m)
 
@@ -21,7 +21,9 @@ Cd_parachute = .85;
 A_parachute = 7.848; %m2
 v_parachute=zeros([1 ripcord]); %m/s
 Fd_parachute=zeros([1 ripcord]);
+x_parachute = zeros([1 ripcord]);
 rho_parachute = zeros([1 ripcord]);
+
 
 %probe values
 x_probe_with_deployment = zeros(1, z); %m
@@ -29,7 +31,7 @@ v_probe_with_deployment = zeros(1,z); %m/s
 a_probe_with_deployment = zeros(1,z); %m/s
 Fd_probe_with_deployment = zeros(1,z);
 rho=zeros(1,z);
-T_cord = 0; 
+T_cord = 0;
 
 %initial values at mission start
 v_probe_with_deployment(1) = 27000;
@@ -37,21 +39,25 @@ Cd_probe=1.05; %drag coeff of probe
 A_probe=.785; %m^2
 m_probe = 355; %kg
 
-
+check = zeros(1,z);
+time = 0:deltat:deltat*z;
 
 for i=1:z
+    
+    current_time = deltat*i;
     
     %probe iteration
     rho(i)=(3000/60000000)*x_probe_with_deployment(i);
     
-    if time_seconds > 3600
-        for l=i:i+ripcord
-        	rho_parachute(l) = rho(i);
-            T_cord = Torque/R_spool;
-            Fd_parachute = T_cord;
-            v_parachute = sqrt((2*Fd_parachute)/(rho_parachute(l)*Cd_parachute*A_parachute));
-        end  
+   if current_time > 3600
+    for l = 1:ripcord
+        rho_parachute(l) = (3000/60000000)*x_parachute(l);
+        T_cord = Torque/R_spool;
+        Fd_parachute = T_cord;
+        v_parachute(l) = sqrt((2*Fd_parachute)/(rho_parachute(l)*Cd_parachute*A_parachute));
+        x_parachute(l) = x_probe_with_deployment(i) + l;
     end
+   end
     
     Fd_probe_with_deployment(i) = 1/2*rho(i)*A_probe*Cd_probe*v_probe_with_deployment(i)^2;
     %force due to drag
@@ -72,27 +78,42 @@ for i=1:z
     x_probe_with_deployment(i+1) = x_probe_with_deployment(i) + v_probe_with_deployment(i)*deltat + (a_probe_with_deployment(i)*deltat^2)/2;
     %current position
     
+    check(i+1) = 0;
+    
+    if current_time > 3600
+        A_probe = 300;
+        check(i+1) = -1000;
+    end
+    
 end
 
-plot(v_probe_with_deployment)
-title('Velocity')
-xlabel('Iterations')
-ylabel('Velocity (m/s)')
+figure
+plot(time, v_probe_with_deployment)
+xlabel('Time (s)')
+ylabel('Probe Velocity (m/s)')
 
 figure
-plot(x_probe_with_deployment)
-title('Position')
-xlabel('Iterations')
-ylabel('Position (m)')
+plot(time, x_probe_with_deployment)
+xlabel('Time (s)')
+ylabel('Probe Position (m)')
+
+figure
+plot(v_parachute)
+xlabel('Ripcord length (m)')
+ylabel('Parachute Velocity (m)')
 
 %%
-figure
-plot(Fd_probe_with_deployment)
-title('Drag')
-
-figure
-plot(a_probe_with_deployment)
-title('Acceleration')
+% figure
+% plot(Fd_probe_with_deployment)
+% title('Drag')
+%
+% figure
+% plot(a_probe_with_deployment)
+% title('Acceleration')
+%
+% figure
+% plot(check)
+% title('Check')
 
 %%
 
